15db50b6e9ea144d7d61b6c3eb4ceb64eb9bf9c8
Edwin Madu Lindblom <edwmad0323@gmail.com>
2026-01-05 14:20:28 +0100

    done with agent and filled database

--- code_alongs/11b_pydantic_fastapi/utils.py
+++ code_alongs/11b_pydantic_fastapi/utils.py
@@ -1,17 +1,16 @@
-#%%
 from pathlib import Path
 import duckdb
 
 DATA_PATH = Path(__file__).parent / "data"
-
 DATA_PATH.mkdir(exist_ok=True)
 
-def query_duckdb(sql_code, parameters = None):
+def query_duckdb(sql_code, parameters=None):
     with duckdb.connect(DATA_PATH / "movies.duckdb") as conn:
-        cursor = conn.execute(sql_code)
+        if parameters:
+            cursor = conn.execute(sql_code, parameters)
+        else:
+            cursor = conn.execute(sql_code)
 
-        sql_code = sql_code.strip().casefold()
-        if sql_code.startswith(("select", "from", "desc", "pragma")):
+        sql_lower = sql_code.strip().casefold()
+        if sql_lower.startswith(("select", "from", "desc", "pragma")):
             return cursor.df()
-
-#%%
